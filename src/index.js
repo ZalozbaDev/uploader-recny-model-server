8b6312ae34c116a05344e63f88a3a90434ffbd19
FS <f.scholze@gmx.de>
2024-02-29 08:17:44 +0100

    Add duration in status

--- src/index.js
+++ src/index.js
@@ -34,16 +34,21 @@ app.get('/status', async (req, res) => {
   if (req.query.token === undefined) {
     res.status(400).send('Token is required')
   }
-  await exec(`cat uploads/${req.query.token}/progress.txt | tail -1`, (error, stdout, stderr) => {
+  await exec(`cat uploads/${req.query.token}/progress.txt`, (error, stdout, stderr) => {
     if (error !== null) {
       console.log(`exec error: ${error}`)
       res.status(400).send('Error')
     }
 
+    const firstLine = stdout.split('\n')[0]
+    const lastLine = stdout.split('\n')[stdout.split('\n').length - 2]
+    const stdoutLinesCount = stdout.split('\n').length
+
     res.send({
-      done: stdout.split('|')[0] === '100',
-      status: stdout.split('|')[0],
-      message: stdout.split('|')[1]
+      duration: stdoutLinesCount <= 2 ? -1 : firstLine, // in seconds
+      done: lastLine.split('|')[0] === '100',
+      status: lastLine.split('|')[0],
+      message: lastLine.split('|')[1]
     })
   })
 })
