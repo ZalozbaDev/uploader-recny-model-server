507f4c16a82768b3566fe450ac924e59aab93a92
Daniel Zoba <daniel.sobe@sorben.com>
2024-02-29 15:37:35 +0100

    fix parameters

--- src/scripts/create_transcript.sh
+++ src/scripts/create_transcript.sh
@@ -37,13 +37,13 @@ case $MODEL in
 		cat $PROGRESS >> $PROGRESS.tmp
 		mv $PROGRESS.tmp $PROGRESS
 		sox $SOURCEFILE.wav -r 16000 -c 1 -b 16 $SOURCEFILE.wav.resample.wav
-		echo "20|Resampling hotowe ($DURATION)" >> $PROGRESS
-		if [ "$OUTFORMAT" = "SRT" ]; then
-			/whisper.cpp/main -m /whisper/hsb/whisper_small/ggml-model.bin --output-txt -f $SOURCEFILE.wav.resample.wav
+		echo "20|Resampling hotowe" >> $PROGRESS
+		if [ "$OUTFORMAT" = "srt" ]; then
+			/whisper.cpp/main -m /whisper/hsb/whisper_small/ggml-model.bin --output-srt -f $SOURCEFILE.wav.resample.wav
 			mv $SOURCEFILE.wav.resample.wav.srt $SOURCEFILE.srt
 			echo "100|Podtitle hotowe" >> $PROGRESS
 		else
-			/whisper.cpp/main -m /whisper/hsb/whisper_small/ggml-model.bin --output-srt -f $SOURCEFILE.wav.resample.wav
+			/whisper.cpp/main -m /whisper/hsb/whisper_small/ggml-model.bin --output-txt -f $SOURCEFILE.wav.resample.wav
 			mv $SOURCEFILE.wav.resample.wav.txt $SOURCEFILE.txt
 			echo "100|Tekst hotowe" >> $PROGRESS
 		fi
@@ -54,7 +54,7 @@ case $MODEL in
 		;;
 	
 	BOZA_MSA)
-		if [ "$OUTFORMAT" = "SRT" ]; then
+		if [ "$OUTFORMAT" = "srt" ]; then
 			echo "0|Wobdźěłam $SOURCEFILE" >> $PROGRESS
 			ffmpeg -i $SOURCEFILE $SOURCEFILE.wav
 			DURATION=$(soxi -D $SOURCEFILE.wav)
@@ -62,7 +62,7 @@ case $MODEL in
 			cat $PROGRESS >> $PROGRESS.tmp
 			mv $PROGRESS.tmp $PROGRESS
 			sox $SOURCEFILE.wav -r 16000 -c 1 -b 16 $SOURCEFILE.wav.resample.wav
-			echo "20|Resampling hotowe ($DURATION)" >> $PROGRESS
+			echo "20|Resampling hotowe" >> $PROGRESS
 			LD_LIBRARY_PATH=/proprietary /proprietary/testrec /proprietary/merged_47_adp.cfg $SOURCEFILE.wav.resample.wav | tee $SOURCEFILE.wav.resample.wav.rec.log
 			echo "80|Spóznawanje hotowe" >> $PROGRESS
 			python3 $(dirname $0)/log2srt.py $SOURCEFILE.wav.resample.wav.rec.log
