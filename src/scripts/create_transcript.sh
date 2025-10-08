8b167ac700b2ee6c953b42b416ae12ac47405bdf
FS <f.scholze@gmx.de>
2025-10-08 11:17:42 +0200

    Add dubbing

--- src/scripts/create_transcript.sh
+++ src/scripts/create_transcript.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-if [ "$#" -ne 5 ]; then
-	echo "need to run this script with the following arguments:"
-	echo "./create_transcript.sh FOLDER SOURCEFILENAME LANGUAGEMODEL OUTPUTFORMAT PROGRESSFILE"
-	echo "Example:"
-	echo "./create_transcript.sh 672536cdbea8737853 video.mp4 whisper srt progress.txt"
-	exit -1
-fi
+
 
 FOLDERNAME=$1
 SOURCEFILE=$2
 MODEL=$3
-OUTFORMAT=$4
-PROGRESS=$5
+OUTFORMAT=srt
+PROGRESS=$4
+TRANSLATE=$5 
+DIARIZATION=$6 
+VAD=$7
 
 
 echo "Dataja=$SOURCEFILE"
@@ -46,17 +43,16 @@ case $MODEL in
 		mv $PROGRESS.tmp $PROGRESS
 		sox $SOURCEFILE.wav -r 16000 -c 1 -b 16 $SOURCEFILE.wav.resample.wav
 		echo "20|Resampling hotowe" >> $PROGRESS
-		if [ "$OUTFORMAT" = "srt" ]; then
-			/whisper.cpp/main -m /whisper/hsb/whisper_small/ggml-model.bin --output-srt -f $SOURCEFILE.wav.resample.wav
-			mv $SOURCEFILE.wav.resample.wav.srt $SOURCEFILE.srt
-			ln -s $(basename $SOURCEFILE.srt) $(echo "${SOURCEFILE%.*}".srt)
-			echo "100|Podtitle hotowe" >> $PROGRESS
-		else
-			/whisper.cpp/main -m /whisper/hsb/whisper_small/ggml-model.bin --output-txt -f $SOURCEFILE.wav.resample.wav
-			mv $SOURCEFILE.wav.resample.wav.txt $SOURCEFILE.text
-			ln -s $(basename $SOURCEFILE.text) $(echo "${SOURCEFILE%.*}".text)
-			echo "100|Tekst hotowe" >> $PROGRESS
-		fi
+		
+		/whisper.cpp/main -m /whisper/hsb/whisper_small/ggml-model.bin --output-srt -f $SOURCEFILE.wav.resample.wav
+		mv $SOURCEFILE.wav.resample.wav.srt $SOURCEFILE.srt
+		ln -s $(basename $SOURCEFILE.srt) $(echo "${SOURCEFILE%.*}".srt)
+		echo "100|Podtitle hotowe" >> $PROGRESS
+		
+		/whisper.cpp/main -m /whisper/hsb/whisper_small/ggml-model.bin --output-txt -f $SOURCEFILE.wav.resample.wav
+		mv $SOURCEFILE.wav.resample.wav.txt $SOURCEFILE.text
+		ln -s $(basename $SOURCEFILE.text) $(echo "${SOURCEFILE%.*}".text)
+		echo "100|Tekst hotowe" >> $PROGRESS
 		;;
 	
 	HFBIG)
@@ -92,17 +88,12 @@ case $MODEL in
 		mv $PROGRESS.tmp $PROGRESS
 		sox $SOURCEFILE.wav -r 16000 -c 1 -b 16 $SOURCEFILE.wav.resample.wav
 		echo "20|Resampling hotowe" >> $PROGRESS
-		if [ "$OUTFORMAT" = "srt" ]; then
-			/whisper.cpp/main -m /whisper/hsb/whisper_small_europeada/ggml-model.bin --output-srt -f $SOURCEFILE.wav.resample.wav
-			mv $SOURCEFILE.wav.resample.wav.srt $SOURCEFILE.srt
-			ln -s $(basename $SOURCEFILE.srt) $(echo "${SOURCEFILE%.*}".srt)
-			echo "100|Podtitle hotowe" >> $PROGRESS
-		else
-			/whisper.cpp/main -m /whisper/hsb/whisper_small_europeada/ggml-model.bin --output-txt -f $SOURCEFILE.wav.resample.wav
-			mv $SOURCEFILE.wav.resample.wav.txt $SOURCEFILE.text
-			ln -s $(basename $SOURCEFILE.text) $(echo "${SOURCEFILE%.*}".text)
-			echo "100|Tekst hotowe" >> $PROGRESS
-		fi
+
+		echo "100|Podtitle hotowe" >> $PROGRESS
+
+
+		echo "100|Tekst hotowe" >> $PROGRESS
+
 		;;
 	
 	FB)
@@ -191,8 +182,10 @@ case $MODEL in
 		mv $PROGRESS.tmp $PROGRESS
 		cp $SOURCEFILE $SOURCEFILE.wav
 		cp $SOURCEFILE.wav $SOURCEFILE.wav.resample.wav
+		cp $SOURCEFILE $SOURCEFILE.wav.resample.wav.rec.log
 		sleep 1
 		echo "20|Resampling hotowe ($DURATION)" >> $PROGRESS
+		cp $SOURCEFILE.wav.resample.wav $SOURCEFILE.wav.resample.wav.rec.log
 		cp $SOURCEFILE.wav.resample.wav $SOURCEFILE.wav.resample.wav.rec.log
 		sleep 5
 		echo "80|Spóznawanje hotowe" >> $PROGRESS
