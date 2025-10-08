3cad584abb4b5ab4a0a2514154deb6328d655141
Daniel Zoba <daniel.sobe@sorben.com>
2025-10-08 11:38:57 +0200

    clean up scripts

--- src/routes/upload/transcript.ts
+++ src/routes/upload/transcript.ts
@@ -62,46 +62,44 @@ export const transcript = (app: Express) => {
   app.get('/models', async (req: Request, res: Response) => {
     res.status(200).send([
       {
-        name: 'HF',
+        name: 'HFHSB',
         title: 'powšitkowny KI model (doporučene)',
-        description:
-          'za powŝitkowne rozmołwy - HuggingFace Whisper "small" wusměrjene na Hornjoserbšćinu',
-        srt: false,
-        diarization: false,
-        vad: false
+        description: 'za powsitkowne rozmołwy',
+        srt: true,
+        diarization: true,
+        vad: true,
+        language: 'hsb',
+        source: 'https://...'
       },
       {
-        name: 'FHG',
-        title: 'powšitkowny klasiski model',
-        description: 'hišće njeje přistupne - Fraunhofer recIKTS',
-        srt: false,
-        diarization: false,
-        vad: false
-      },
-      {
-        name: 'EURO',
-        title: 'KI model Europeady 2022',
-        description:
-          'za transkript Europeady - HuggingFace Whisper "base" wusměrjene na hry Europeady 2022',
-        srt: false,
-        diarization: false,
-        vad: false
-      },
-      {
-        name: 'FB',
-        title: 'wjacerěčny KI model',
-        description: 'za powŝitkowne rozmołwy - Facebook MMS',
-        srt: false,
-        diarization: false,
-        vad: false
+        name: 'HFDSB',
+        title: 'powšitkowny KI model (dokladne, pomalu)',
+        description: 'za powsitkowne rozmołwy',
+        srt: true,
+        diarization: true,
+        vad: true,
+        language: 'dsb',
+        source: 'https://...'
       },
       {
         name: 'BOZA_MSA',
         title: 'klasiski model za bože mšě',
         description: 'wusměrjene na cyrkwinsku rěč - Fraunhofer recIKTS',
-        srt: false,
+        srt: true,
         diarization: false,
-        vad: false
+        vad: true
+        language: 'hsb',
+        source: 'Fraunhofer recIKTS'
+      },
+      {
+        name: 'HFDE',
+        title: 'powšitkowny KI model (dokladne, pomalu)',
+        description: 'za powsitkowne rozmołwy',
+        srt: true,
+        diarization: true,
+        vad: true,
+        language: 'de',
+        source: 'https://...'
       },
       {
         name: 'DEVEL',
@@ -109,7 +107,9 @@ export const transcript = (app: Express) => {
         description: 'jenož za wuwiwarjow',
         srt: false,
         diarization: false,
-        vad: false
+        vad: false,
+        language: 'hsb',
+        source: 'xxx'
       }
     ])
   })
--- src/scripts/create_transcript.sh
+++ src/scripts/create_transcript.sh
@@ -17,24 +17,20 @@ echo "Postup=$PROGRESS"
 echo "Rjadowak=$FOLDERNAME"
 echo "Model=$MODEL"
 echo "Format=$OUTFORMAT"
+echo "Translate=$TRANSLATE"
+echo "DiarizeSpeakers=$DIARIZATION"
+echo "VAD=$VAD"
 
 touch $PROGRESS
 
 # list all currently used models here
 
 RECIKTS_MODEL_BOZA_MSA=misa_2024_08_02.cfg
-RECIKTS_MODEL_GMEJNSKE=gmejnske_2024_08_21.cfg
-
-# TBD whisper models
 WHISPER_MODEL_GERMAN=large-v2
 
 case $MODEL in
 
-	FHG)
-		echo "100|Tuta warianta hišće njeje přistupna!" >> $PROGRESS
-		;;
-	
-	HF)
+	HFHSB)
 		echo "0|Wobdźěłam $SOURCEFILE" >> $PROGRESS
 		ffmpeg -i $SOURCEFILE $SOURCEFILE.wav
 		DURATION=$(soxi -D $SOURCEFILE.wav)
@@ -90,53 +86,6 @@ case $MODEL in
 		fi
 		;;
 	
-	EURO)
-		echo "0|Wobdźěłam $SOURCEFILE" >> $PROGRESS
-		ffmpeg -i $SOURCEFILE $SOURCEFILE.wav
-		DURATION=$(soxi -D $SOURCEFILE.wav)
-		echo ${DURATION%.*} > $PROGRESS.tmp # strip the decimal part
-		cat $PROGRESS >> $PROGRESS.tmp
-		mv $PROGRESS.tmp $PROGRESS
-		sox $SOURCEFILE.wav -r 16000 -c 1 -b 16 $SOURCEFILE.wav.resample.wav
-		echo "20|Resampling hotowe" >> $PROGRESS
-		if [ "$OUTFORMAT" = "srt" ]; then
-			/whisper.cpp/build/bin/whisper-cli -m /whisper/hsb/whisper_small_europeada/ggml-model.bin --output-srt -f $SOURCEFILE.wav.resample.wav
-			mv $SOURCEFILE.wav.resample.wav.srt $SOURCEFILE.srt
-			ln -s $(basename $SOURCEFILE.srt) $(echo "${SOURCEFILE%.*}".srt)
-			echo "100|Podtitle hotowe" >> $PROGRESS
-		else
-			/whisper.cpp/build/bin/whisper-cli -m /whisper/hsb/whisper_small_europeada/ggml-model.bin --output-txt -f $SOURCEFILE.wav.resample.wav
-			mv $SOURCEFILE.wav.resample.wav.txt $SOURCEFILE.text
-			ln -s $(basename $SOURCEFILE.text) $(echo "${SOURCEFILE%.*}".text)
-			echo "100|Tekst hotowe" >> $PROGRESS
-		fi
-		;;
-	
-	FB)
-		if [ "$OUTFORMAT" = "text" ]; then
-			echo "0|Wobdźěłam $SOURCEFILE" >> $PROGRESS
-			ffmpeg -i $SOURCEFILE $SOURCEFILE.wav
-			DURATION=$(soxi -D $SOURCEFILE.wav)
-			echo ${DURATION%.*} > $PROGRESS.tmp # strip the decimal part
-			cat $PROGRESS >> $PROGRESS.tmp
-			mv $PROGRESS.tmp $PROGRESS
-			sox $SOURCEFILE.wav -r 16000 -c 1 -b 16 $SOURCEFILE.wav.resample.wav
-			echo "20|Resampling hotowe" >> $PROGRESS
-			echo "test test test" > $SOURCEFILE.trl.resample.trl
-			export USER=$FOLDERNAME
-			pushd /fairseq
-			source bin/activate
-			python /fairseq/examples/mms/asr/infer/mms_infer.py --model /fairseqdata/mms1b_all.pt  --lang hsb --audio /uploader-recny-model-server/$SOURCEFILE.wav.resample.wav --format letter > /uploader-recny-model-server/$SOURCEFILE.log
-			popd
-			echo "80|Spóznawanje hotowe" >> $PROGRESS
-			mv $SOURCEFILE.log ${SOURCEFILE}.${OUTFORMAT}
-			ln -s $(basename $SOURCEFILE.text) $(echo "${SOURCEFILE%.*}".text)
-			echo "100|Podtitle hotowe" >> $PROGRESS
-		else
-			echo "100|Tuta warianta hišće njeje přistupna!" >> $PROGRESS
-		fi
-		;;
-	
 	BOZA_MSA)
 		echo "0|Wobdźěłam $SOURCEFILE" >> $PROGRESS
 		ffmpeg -i $SOURCEFILE $SOURCEFILE.wav
@@ -163,32 +112,31 @@ case $MODEL in
 		fi
 		;;
 		
-	GMEJ)
-		echo "0|Wobdźěłam $SOURCEFILE" >> $PROGRESS
-		ffmpeg -i $SOURCEFILE $SOURCEFILE.wav
-		DURATION=$(soxi -D $SOURCEFILE.wav)
-		echo ${DURATION%.*} > $PROGRESS.tmp # strip the decimal part
-		cat $PROGRESS >> $PROGRESS.tmp
-		mv $PROGRESS.tmp $PROGRESS
-		sox $SOURCEFILE.wav -r 16000 -c 1 -b 16 $SOURCEFILE.wav.resample.wav
-		echo "20|Resampling hotowe" >> $PROGRESS
-		LD_LIBRARY_PATH=/proprietary /proprietary/testrec /proprietary/$RECIKTS_MODEL_GMEJNSKE $SOURCEFILE.wav.resample.wav | tee $SOURCEFILE.wav.resample.wav.rec.log
-		echo "80|Spóznawanje hotowe" >> $PROGRESS
-		if [ "$OUTFORMAT" = "srt" ]; then
-			python3 $(dirname $0)/log2srt.py $SOURCEFILE.wav.resample.wav.rec.log
-			mv uploads/${FOLDERNAME}/*.srt ${SOURCEFILE}.${OUTFORMAT}
-			ln -s $(basename $SOURCEFILE.srt) $(echo "${SOURCEFILE%.*}".srt)
-			echo "100|Podtitle hotowe" >> $PROGRESS
-			echo "----> HOTOWE <----"
-		else
-			python3 $(dirname $0)/log2txt.py $SOURCEFILE.wav.resample.wav.rec.log
-			mv uploads/${FOLDERNAME}/*.rawtxt ${SOURCEFILE}.${OUTFORMAT}
+	FB)
+		if [ "$OUTFORMAT" = "text" ]; then
+			echo "0|Wobdźěłam $SOURCEFILE" >> $PROGRESS
+			ffmpeg -i $SOURCEFILE $SOURCEFILE.wav
+			DURATION=$(soxi -D $SOURCEFILE.wav)
+			echo ${DURATION%.*} > $PROGRESS.tmp # strip the decimal part
+			cat $PROGRESS >> $PROGRESS.tmp
+			mv $PROGRESS.tmp $PROGRESS
+			sox $SOURCEFILE.wav -r 16000 -c 1 -b 16 $SOURCEFILE.wav.resample.wav
+			echo "20|Resampling hotowe" >> $PROGRESS
+			echo "test test test" > $SOURCEFILE.trl.resample.trl
+			export USER=$FOLDERNAME
+			pushd /fairseq
+			source bin/activate
+			python /fairseq/examples/mms/asr/infer/mms_infer.py --model /fairseqdata/mms1b_all.pt  --lang hsb --audio /uploader-recny-model-server/$SOURCEFILE.wav.resample.wav --format letter > /uploader-recny-model-server/$SOURCEFILE.log
+			popd
+			echo "80|Spóznawanje hotowe" >> $PROGRESS
+			mv $SOURCEFILE.log ${SOURCEFILE}.${OUTFORMAT}
 			ln -s $(basename $SOURCEFILE.text) $(echo "${SOURCEFILE%.*}".text)
-			echo "100|Tekst hotowe" >> $PROGRESS
-			echo "----> HOTOWE <----"
+			echo "100|Podtitle hotowe" >> $PROGRESS
+		else
+			echo "100|Tuta warianta hišće njeje přistupna!" >> $PROGRESS
 		fi
 		;;
-		
+	
 	DEVEL)
 		echo "0|Wobdźěłam $SOURCEFILE" >> $PROGRESS
 		sleep 1
@@ -268,28 +216,6 @@ case $MODEL in
 		sox $SOURCEFILE.wav -r 48000 -c 1 -b 16 $SOURCEFILE.wav.resample.wav
 		echo "20|Resampling hotowe" >> $PROGRESS
 		LD_LIBRARY_PATH=/proprietary /recikts_main /proprietary/$RECIKTS_MODEL_BOZA_MSA $SOURCEFILE.wav.resample.wav ./uploads/${FOLDERNAME} > ./uploads/${FOLDERNAME}/log.txt 2>&1
-		echo "80|Spóznawanje hotowe" >> $PROGRESS
-		if [ "$OUTFORMAT" = "srt" ]; then
-			mv uploads/${FOLDERNAME}/subtitles.srt ${SOURCEFILE}.${OUTFORMAT}
-			ln -s $(basename $SOURCEFILE.srt) $(echo "${SOURCEFILE%.*}".srt)
-			echo "100|Podtitle hotowe" >> $PROGRESS
-		else
-			mv uploads/${FOLDERNAME}/transcript.txt ${SOURCEFILE}.${OUTFORMAT}
-			ln -s $(basename $SOURCEFILE.text) $(echo "${SOURCEFILE%.*}".text)
-			echo "100|Tekst hotowe" >> $PROGRESS
-		fi
-		;;
-		
-	GMEJ_PAU)
-		echo "0|Wobdźěłam $SOURCEFILE" >> $PROGRESS
-		ffmpeg -i $SOURCEFILE $SOURCEFILE.wav
-		DURATION=$(soxi -D $SOURCEFILE.wav)
-		echo ${DURATION%.*} > $PROGRESS.tmp # strip the decimal part
-		cat $PROGRESS >> $PROGRESS.tmp
-		mv $PROGRESS.tmp $PROGRESS
-		sox $SOURCEFILE.wav -r 48000 -c 1 -b 16 $SOURCEFILE.wav.resample.wav
-		echo "20|Resampling hotowe" >> $PROGRESS
-		LD_LIBRARY_PATH=/proprietary /recikts_main /proprietary/$RECIKTS_MODEL_GMEJNSKE $SOURCEFILE.wav.resample.wav ./uploads/${FOLDERNAME} > ./uploads/${FOLDERNAME}/log.txt 2>&1
 		echo "80|Spóznawanje hotowe" >> $PROGRESS
 		if [ "$OUTFORMAT" = "srt" ]; then
 			mv uploads/${FOLDERNAME}/subtitles.srt ${SOURCEFILE}.${OUTFORMAT}
