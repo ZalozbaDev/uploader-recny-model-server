f28bbf0ff3ec7073eecf89406ba106a7b282d099
Daniel Zoba <daniel.sobe@sorben.com>
2025-10-09 14:00:51 +0200

    add option for diarization

--- docker/startme_transcription.sh
+++ docker/startme_transcription.sh
@@ -5,5 +5,6 @@ echo "PORT_TRANSCRIPT=$PORT_TRANSCRIPT" > .env
 popd
 
 echo "SOTRA_URL set to $SOTRA_URL"
+echo "HF_TOKEN set to $HF_TOKEN"
 
 cd uploader-recny-model-server && npm run start:transcript
