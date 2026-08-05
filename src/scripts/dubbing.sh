23338e6a8f9c90e8520414b8f224cc07b8967465
Daniel Zoba <d.zoba@zalozba.de>
2026-08-05 13:43:17 +0200

    work on images and improve dubbing

--- src/scripts/dubbing.sh
+++ src/scripts/dubbing.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-pushd /open-dubbing
+# OPENDUBBING_DIR=/open-dubbing
+OPENDUBBING_DIR=/opt/venv/open-dubbing
+
+# OPENDUBBING_DEVICE=cpu
+OPENDUBBING_DEVICE=cuda
+
+
+pushd ${OPENDUBBING_DIR}
 source bin/activate
 
 echo "open-dubbing called with args $1 , $2 , $3"
@@ -19,7 +26,7 @@ if [ "$#" -eq 3 ]; then
 	--translator sotra --apertium_server $SOTRA_URL \
 	--tts bamborak --tts_api_server https://bamborakapi.mudrowak.de/api/tts/ \
 	--dubbed_subtitles --original_subtitles --log_level DEBUG --input_srt $SUBSFILE \
-	--device cpu
+	--device ${OPENDUBBING_DEVICE}
 else
 	echo "do not have SRT"
 	open-dubbing --input_file $FILENAME --source_language deu --target_language hsb \
@@ -27,7 +34,7 @@ else
 	--translator sotra --apertium_server $SOTRA_URL \
 	--tts bamborak --tts_api_server https://bamborakapi.mudrowak.de/api/tts/ \
 	--dubbed_subtitles --original_subtitles --log_level DEBUG \
-	--device cpu
+	--device ${OPENDUBBING_DEVICE}
 fi
 
 echo "open-dubbing DONE"
